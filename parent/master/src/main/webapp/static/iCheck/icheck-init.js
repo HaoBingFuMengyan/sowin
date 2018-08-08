95bf9ef152f93d16cca6c617fd15c465f9ed6668
haobingfu <haobingfu@frogsing.com>
2018-08-08 15:18:08 +0800

    commit modify

---
+++ parent/master/src/main/webapp/static/iCheck/icheck-init.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.i-checks').iCheck({
+		checkboxClass : 'icheckbox_square-green',
+		radioClass : 'iradio_square-green',
+	});
+	
+	$("thead input.i-checks").on('ifChecked', function(event){ //ifCreated 事件应该在插件初始化之前绑定 
+		$("input.i-checks").iCheck('check');
+    	});
+
+	$("thead input.i-checks").on('ifUnchecked', function(event){ //ifCreated 事件应该在插件初始化之前绑定 
+		$("input.i-checks").iCheck('uncheck');
+    	});
+});
